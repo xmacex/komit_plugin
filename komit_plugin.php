d0d68223001bfc9e78bf4f2666750678a9a49737
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2017-10-03 13:44:32 +0200

    Updated the plugin to deal with the University of Copenhagen server which seems to require HTTP User-Agent header, or otherwise it gives an 400 Bad Request error

--- komit_plugin.php
+++ komit_plugin.php
@@ -2,15 +2,22 @@
 /*
  Plugin Name: Event list of MCC
  Description: Produce a listing of events at KU MCC
- Version: 0.1
+ Version: 0.2
  Author: Mace Ojala
  Licence: GPL3
  Licence URI: https://www.gnu.org/licences/gpl.html
 */
 
 function listevents() {
+    // Get events and form a list of them
+    $context = stream_context_create(array(
+        'http' => array(
+            'header' => 'User-Agent: komit_plugin'
+        )
+    ));
     $url = "http://mcc.ku.dk/calendar/?get_ical=1";
-    $response = file_get_contents($url);
+    
+    $response = file_get_contents($url, false, $context);
     $lines = explode("\n", $response);
 
     // Produce a HTML list
